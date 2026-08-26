4e647d1b3d6ada1dcd5918b9047bdccdf7130da7
Mikah <H4k1m135899999@gmail.com>
2026-08-26 10:16:25 -0300

    adicionando teste

--- teste.py
+++ teste.py
@@ -2,4 +2,13 @@ from robo import Robo
 
 robo = Robo()
 
-robo.andar(1000, 0.4)
+while True:
+    #robo.andar(1000, 0.4)
+
+    #robo.girar(90)
+
+    robo.set_motores(1,0)
+
+    #robo.set_motores(0,1)
+
+    #robo.tras(0.3)
