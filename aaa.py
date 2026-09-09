feb4ae126fa5ed919f592507fde551fd3fae7c78
Mikah <H4k1m135899999@gmail.com>
2026-09-09 15:32:56 -0300

    teste da garra

--- aaa.py
+++ aaa.py
@@ -1,9 +1,27 @@
 #from abc import executar_captura
 from setup import servo_claw, servo_lift, servo_dump
+from time import sleep
 
 #executar_captura()
 
-#servo_claw.min() #Braço inteiro
+servo_claw.min()
+sleep(0.5)
+
+servo_claw.max()
+sleep(0.5)
+
+servo_lift.min()
+sleep(0.5)
 
 servo_lift.max()
+sleep(0.5)
+
+servo_dump.min()
+sleep(0.5)
+
+servo_dump.max()
+sleep(0.5)
+
+#servo_claw.min() #Braço inteiro
+#servo_lift.max()
 #servo_dump.max()
