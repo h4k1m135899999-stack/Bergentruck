e9d080dc447a60261d5ae83acfada13edfb54d4c
Mikah <H4k1m135899999@gmail.com>
2026-09-09 15:15:06 -0300

    atualizaÃ§Ã£o

--- aaa.py
+++ aaa.py
@@ -5,8 +5,6 @@ from setup import servo_claw, servo_lift, servo_dump
 
 while True:
     servo_claw.max() #Braço inteiro
-    servo_claw.min()
-    servo_claw.mid()
 
 #servo_lift.mid()
 #servo_dump.max()
