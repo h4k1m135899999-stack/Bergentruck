42727d72c84b3e60c7fbf558e7918f00c967b846
Mikah <H4k1m135899999@gmail.com>
2026-09-09 16:44:48 -0300

    ble ble ble

--- aaa.py
+++ aaa.py
@@ -4,24 +4,25 @@ from time import sleep
 
 #executar_captura()
 while True:
+    '''
     servo_claw.min()
     sleep(0.5)
 
     servo_claw.max()
     sleep(0.5)
-
+    '''
     servo_lift.min()
     sleep(0.5)
 
     servo_lift.max()
     sleep(0.5)
-
+    ''' 
     servo_dump.min()
     sleep(0.5)
 
     servo_dump.max()
     sleep(0.5)
-
+    '''
 
 #servo_claw.min() #Braço inteiro
 #servo_lift.max()
