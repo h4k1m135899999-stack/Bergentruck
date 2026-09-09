ac8367aa7eb1b55c8059f07851061ccb0d69515c
Mikah <H4k1m135899999@gmail.com>
2026-09-09 16:42:13 -0300

    não sei oq the victory

--- aaa.py
+++ aaa.py
@@ -3,26 +3,25 @@ from setup import servo_claw, servo_lift, servo_dump
 from time import sleep
 
 #executar_captura()
+while True:
+    servo_claw.min()
+    sleep(0.5)
 
-'''
-servo_claw.min()
-sleep(0.5)
+    servo_claw.max()
+    sleep(0.5)
 
-servo_claw.max()
-sleep(0.5)
-'''
-servo_lift.min()
-sleep(0.5)
+    servo_lift.min()
+    sleep(0.5)
 
-servo_lift.max()
-sleep(0.5)
-'''
-servo_dump.min()
-sleep(0.5)
+    servo_lift.max()
+    sleep(0.5)
 
-servo_dump.max()
-sleep(0.5)
-'''
+    servo_dump.min()
+    sleep(0.5)
+
+    servo_dump.max()
+    sleep(0.5)
+
 
 #servo_claw.min() #Braço inteiro
 #servo_lift.max()
