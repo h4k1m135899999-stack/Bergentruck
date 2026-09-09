208c49593a522ebe636e29fac763369148329580
Mikah <H4k1m135899999@gmail.com>
2026-09-09 15:10:39 -0300

    atualizaÃ§Ã£o

--- aaa.py
+++ aaa.py
@@ -3,6 +3,6 @@ from setup import servo_claw, servo_lift, servo_dump
 
 #executar_captura()
 
-servo_claw.min()
-servo_lift.mid()
-servo_dump.max()
+servo_claw.max() #Braço inteiro
+#servo_lift.mid()
+#servo_dump.max()
